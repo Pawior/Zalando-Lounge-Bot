f65f0fa18f930543a5c5ffd44a8e7f3e3a9e1613
Pawior <pawiorx@gmail.com>
2022-02-16 01:10:49 +0100

    Working adding to basket products with size=m

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const puppeteer = require("puppeteer");
 // A wcześniej stworzyłem folder user_data i skopiowałem do niego pliki z chromium normalnego na którym zalogowałem się do lounge
 // A jak się zalogowałem do lounge'a to lounge stworzył localstorage na podstawie którego odbywa się logowanie
 // Ten localstorage jest tu najważniejszym plikiem który potrzebuje puppeteer przy starcie
+const campaign = "ZZO1MU8";
+let productsArr;
 (async () => {
   const browser = await puppeteer.launch({
     headless: false,
@@ -16,16 +18,63 @@ const puppeteer = require("puppeteer");
     height: 1080,
     deviceScaleFactor: 1,
   });
-  await page.goto("https://www.zalando-lounge.pl", {
+  await page.goto(`https://www.zalando-lounge.pl/campaigns/${campaign}`, {
     waitUntil: "networkidle0",
   });
-  await page.waitForTimeout(12000);
+
   //   await page.evaluate(() => {
   //     location.reload(true);
   //   });
 
   //   await page.reload({ waitUntil: ["networkidle0", "domcontentloaded"] });
-  await page.screenshot({ path: "example.png" });
 
+  // let productsArr = document.querySelectorAll("vMpOO");
+
+  // //   console.log(productsArr);
+  // await page.screenshot({ path: "example.png" });
+  // for (let i = 0; i < 5; i++) {
+  //   // await Promise.all([page.waitForNavigation(), page.click(productsArr[i])]);
+  //   // await page.goBack();
+  //   console.log(i);
+  //   console.log(campaign);
+  //   // await productsArr[i].click();
+  //   // await page.$eval(selectorString, elem => (elem as HTMLElement).click());
+  //   // await Promise.all([page.click(productsArr[i]), page.waitForNavigation()]);
+  //   await productsArr[i].click();
+  //   console.log("xd");
+  //   await page.waitForTimeout(1500);
+  //   console.log("no i cyh");
+  //   // await page.waitForTimeout(500);
+  //   await page.goBack();
+  // }
+
+  //   await page.$$eval(".vMpOO", (elHandles) =>
+  //     elHandles.forEach((el) => {
+  //       el.click();
+  //       page.waitForNavigation();
+  //       page.waitForTimeout(1500);
+  //       page.goBack();
+  //     })
+  //   );
+
+  let productsArr = await page.$$(".vMpOO");
+
+  console.log(productsArr);
+  for (let i = 0; i < 20; i++) {
+    await productsArr[i].click();
+    // await page.waitForTimeout(1500);
+    await page.waitForSelector(".jIVZOs");
+    let aElementsWithHi = await page.$x("//span[contains(., 'M')]");
+    await aElementsWithHi[0].click();
+    // await page.click(".jIVZOs");
+    let aElementsWithKoszyk = await page.$x(
+      "//span[contains(., 'Do koszyka')]"
+    );
+    await aElementsWithKoszyk[0].click();
+    await page.goBack();
+    productsArr = await page.$$(".vMpOO");
+    // Po wejsciu rozmiar - ten button class który nie jest disabled
+    // 2 opcja: click jIVZOs
+  }
   await browser.close();
 })();
