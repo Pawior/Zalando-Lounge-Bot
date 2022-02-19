ff800e47699cfbda78c460e1c0c18cfd136bcc25
Pawior <pawiorx@gmail.com>
2022-02-19 19:00:35 +0100

    nearly... working

--- index.js
+++ index.js
@@ -5,22 +5,32 @@ const puppeteer = require("puppeteer");
 // A wcześniej stworzyłem folder user_data i skopiowałem do niego pliki z chromium normalnego na którym zalogowałem się do lounge
 // A jak się zalogowałem do lounge'a to lounge stworzył localstorage na podstawie którego odbywa się logowanie
 // Ten localstorage jest tu najważniejszym plikiem który potrzebuje puppeteer przy starcie
-const campaign = "ZZO1P28";
+const campaign = "ZZO1MYN";
 let productsArr;
 (async () => {
   const browser = await puppeteer.launch({
+    ignoreHTTPSErrors: true,
     headless: false,
-    userDataDir: "./user_data",
+    userDataDir: "./user_Data",
   });
+  // const client = await page.target().createCDPSession();
+  // await client.send('Network.clearBrowserCookies');
+  // await client.send('Network.clearBrowserCache');
   const page = await browser.newPage();
+  // const context = await browser.createIncognitoBrowserContext();
+  // const page = await context.newPage();
   await page.setViewport({
     width: 1920,
     height: 1080,
     deviceScaleFactor: 1,
   });
-  await page.goto(`https://www.zalando-lounge.pl/campaigns/${campaign}`, {
-    waitUntil: "networkidle0",
-  });
+  await page.goto(`https://www.zalando-lounge.pl/campaigns/${campaign}`)
+  // await page.waitForNavigation({
+  //   waitUntil: 'networkidle0'
+  // });
+  // await page.goto(`https://www.zalando-lounge.pl/campaigns/${campaign}`, {
+  //   waitUntil: "networkidle0",
+  // });
   // await page.waitForTimeout(2000)
 
   //   await page.evaluate(() => {
@@ -61,11 +71,10 @@ let productsArr;
   let productsArr = await page.$$(".vMpOO");
 
   // console.log(productsArr);
-  for (let i = 0; i < 5; i++) {
+  for (let i = 6; i < 20; i++) {
     await productsArr[i].click();
     // await page.waitForTimeout(1500);
     await page.waitForSelector(".jIVZOs");
-    await page.waitForTimeout(200)
     // let aElementsWithHi;
     // let mSizeElements = await page.$x("//span[contains(., 'M')]");
     // let sSizeElements = await page.$x("//span[contains(., 'S')]");
@@ -81,49 +90,96 @@ let productsArr;
     //   await lSizeElements[0].click();
     // }
 
+
+
     // const [span] = await page.$x("//button[@class='kTLayg']/span[contains(., 'M')]");
-    var mSizeElements = await page.$x("//span[text()='M']");
-    var sSizeElements = await page.$x("//span[text()='S']");
-    var lSizeElements = await page.$x("//span[text()='L']");
-    var xlSizeElements = await page.$x("//span[text()='XL']");
-    // console.log(lSizeElements[0])
-    // var testIt = await page.waitForXPath("//span[text()='M']")
-    // console.log(testIt)
+
+
+    // var mSizeElements = await page.$x("//span[text()='M']");
+    // var sSizeElements = await page.$x("//span[text()='S']");
+    // var lSizeElements = await page.$x("//span[text()='L']");
+    // var xlSizeElements = await page.$x("//span[text()='XL']");
+    // var xsSizeElements = await page.$x("//span[text()='XS']");
+    // await page.waitForTimeout(1000)
+    // console.log(sSizeElements[0])
+
+    // if (mSizeElements.length > 0) {
+    //   await page.waitForTimeout(300)
+    //   await mSizeElements[0].click();
+    //   console.log("klik \n")
+    //   await page.waitForTimeout(300)
+    // }
+    // if (sSizeElements.length > 0) {
+
+    //   await sSizeElements[0].click();
+    //   console.log("klik \n")
+
+    // }
+    // if (lSizeElements.length > 0) {
+
+    //   await lSizeElements[0].click();
+    //   console.log("klik \n")
+
+    // }
+    // if (xlSizeElements.length > 0) {
+
+    //   await xlSizeElements[0].click();
+    //   console.log("klik \n")
+
+    // }
+    // if (xsSizeElements.length > 0) {
+
+    //   await xsSizeElements[0].click();
+    //   console.log("klik \n")
+
+    // }
+    /**============================================
+     *                Skurwysynu click
+     *=============================================**/
+    // await page.$eval("button[class='styles__ArticleSizeButton-sc-1n1fwgw-0 jIVZOs'", elem => elem.click());
+    var btn = await page.$("button[class='styles__ArticleSizeButton-sc-1n1fwgw-0 jIVZOs'")
+    // var spanik = await page.evaluateHandle(e => e.children, btn);
+    // let value = await spanik.evaluate(el => el.textContent)
+    let valueBtn = await btn.evaluate(el => el.textContent)
+    // console.log(value)
+    console.log(valueBtn)
+    console.log("\n \n \n \n \n \n \n \n \n \n \n")
+
+    await page.waitForTimeout(100)
+    // =================================================================
+    const btns = await page.$$("button[class='styles__ArticleSizeButton-sc-1n1fwgw-0 jIVZOs'")
+    await page.waitForTimeout(100)
+    for (let i = 0; i < btns.length; i++) {
+      const tweet = await (await btns[i].getProperty('innerText')).jsonValue();
+      console.log(tweet);
+      if (tweet.includes("M")) {
+        btns[i].click();
+      } else if (tweet.includes("S") && !tweet.includes("XS") && !tweet.includes("XXS")) {
+        btns[i].click();
+      }
+    }
+
+
+
+    // const data = await page.$eval("//span[text()='M']", el => {
+    //   el.click()
+    // });
+    // await page.waitForTimeout(200)
+    // const example = await page.evaluate(element => {
+    //   element[0].click();
+    // }, (await page.$x('//span[text()="M"]'))[0]);
+
+
+
     await page.waitForTimeout(200)
-    // await mSizeElements[0].click();
-
-    // await lSizeElements[0].click();
-    // await mSizeElements[0].click();
-    // await xlSizeElements[0].click();
-    // await lSizeElements[0].click();
-    // await sSizeElements[0].click();
-
-    if (mSizeElements.length > 0) {
-
-      await mSizeElements[0].click();
-    }
-    if (lSizeElements.length > 0) {
-
-      await lSizeElements[0].click();
-    }
-    if (xlSizeElements.length > 0) {
-
-      await xlSizeElements[0].click();
-    }
-    if (sSizeElements.length > 0) {
-
-      await sSizeElements[0].click();
-    }
-
-
-    // await aElementsWithHi[0].click();
-    // await page.click(".jIVZOs");
-    let aElementsWithKoszyk = await page.$x(
-      "//span[contains(., 'Do koszyka')]"
-    );
-    await aElementsWithKoszyk[0].click();
+    // let aElementsWithKoszyk = await page.$x(
+    //   "//span[contains(., 'Do koszyka')]"
+    // );
+    // await aElementsWithKoszyk[0].click();
+    await page.$eval("button[class='styles__Wrapper-sc-causv5-0 jelrHA auto-tests-add-to-cart-button articleButton___add-to-cart___25lRs core___flipper___2fuhC'", elem => elem.click());
     await page.goBack();
     productsArr = await page.$$(".vMpOO");
+    await page.waitForTimeout(200)
     // Po wejsciu rozmiar - ten button class który nie jest disabled
     // 2 opcja: click jIVZOs
   }
